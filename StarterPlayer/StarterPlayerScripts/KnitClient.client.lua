cfa73ab1d8b8b5fcde963d5b9ced1e7ef0454603
BigBoga <103874846+BigBoga@users.noreply.github.com>
2024-05-03 09:11:29 +0300

    Delete StarterPlayer directory

--- StarterPlayer/StarterPlayerScripts/KnitClient.client.lua
+++
@@ -1,5 +0,0 @@
-local ReplicatedStorage = game:GetService("ReplicatedStorage")
-local Knit = require(ReplicatedStorage.Packages.Knit)
-
-Knit.AddControllers(ReplicatedStorage.Controllers)  -- add folder with controllers
-Knit.Start():catch(warn("started")) -- starting knit
