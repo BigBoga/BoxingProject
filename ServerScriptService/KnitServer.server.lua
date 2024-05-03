28b8d19fd2bfe1de76a669a273a47750ba8649c7
BigBoga <103874846+BigBoga@users.noreply.github.com>
2024-05-03 09:10:52 +0300

    Delete ServerScriptService directory

--- ServerScriptService/KnitServer.server.lua
+++
@@ -1,5 +0,0 @@
-local ServerStorage = game:GetService("ServerStorage")
-local Knit = require(game:GetService("ReplicatedStorage").Packages.Knit)
-
-Knit.AddServices(ServerStorage.Services) -- add services 
-Knit.Start():catch(warn("started")) -- starting knit
